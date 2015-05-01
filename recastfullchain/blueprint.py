61b1a54c78ccd08509eaf6c321a32d52e2c16dd8
Lukas <lukas.heinrich@gmail.com>
2015-05-01 16:49:43 -0400

    Update blueprint.py

--- recastfullchain/blueprint.py
+++ recastfullchain/blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request
-blueprint = Blueprint('susyhiggs_analysis', __name__, template_folder='templates')
+blueprint = Blueprint('fullchain_analysis', __name__, template_folder='templates')
 
 RECAST_ANALYSIS_ID = '19c471ff-2514-eb44-0d82-59563cc38dab'
 
